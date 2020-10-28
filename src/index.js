52bb9d1af46fab8fee1712ce4d6478cb8435ad1f
João Bispo <vitor.vi10@hotmail.com>
2020-10-28 20:17:28 -0300

    :paperclip: chore: createed and connected to database

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const config = require('./config/config');
 
 const app = express();
 
 const routes = require('./routes');
+
+mongoose.connect(config.mongodb_uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('connected at uploadfy database'))
+    .catch((err) => console.log('error to connect to database', err));
+
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
+
 
 app.use(routes);
 
