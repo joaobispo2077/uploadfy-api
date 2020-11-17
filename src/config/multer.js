83b7623551cf67af71741f71ed7c7b75548b8cb9
João Bispo <vitor.vi10@hotmail.com>
2020-11-17 15:58:30 -0300

    :paperclip: chore: remove files inside bucket s3 before remove image data

--- src/config/multer.js
+++ src/config/multer.js
@@ -44,7 +44,7 @@ const storageTypes = {
 
 module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: storageTypes['local'],
+  storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
